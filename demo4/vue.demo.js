204fdb8b407983a02dc82a4b268f1d091ebe9850
Jarvis Zhang <345054597@qq.com>
2017-04-23 11:05:37 +0800

    4

---
+++ demo4/vue.demo.js
@@ -0,0 +1,9 @@
+var data={
+    message:'Hello Vue!!',
+    messageArray:['Hello World!!','Hello Vue!!','Vue'],
+    messageObj:{a:'Hello World!!',b:'Hello Vue!!',c:'Yahaha!!'},
+};
+var app=new Vue({
+    el:'#app',
+    data:data,
+});
